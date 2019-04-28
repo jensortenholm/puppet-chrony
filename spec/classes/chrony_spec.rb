182f55f8bc4cd026856a78f323780eee5958d64a
jorten <jorten@mm.st>
2019-04-28 16:16:42 +0200

    Cleanups

--- spec/classes/chrony_spec.rb
+++ spec/classes/chrony_spec.rb
@@ -6,37 +6,48 @@ describe 'chrony' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
-      it { should contain_file('/etc/chrony.conf').with(
-        :ensure => 'file',
-        :owner  => 'root',
-        :group  => 'root',
-        :mode   => '0644',
-      )}
+      it {
+        is_expected.to contain_file('/etc/chrony.conf').with(
+          ensure: 'file',
+          owner:  'root',
+          group:  'root',
+          mode:   '0644',
+        )
+      }
 
       ['present', 'absent'].each do |pkg_status|
         context "with package_ensure #{pkg_status}" do
-          let(:params) { { :package_ensure => pkg_status } }
-          it { should contain_package('chrony').with(
-            :ensure => pkg_status,
-          )}
+          let(:params) { { package_ensure: pkg_status } }
+
+          it {
+            is_expected.to contain_package('chrony').with(
+              ensure: pkg_status,
+            )
+          }
         end
       end
 
       ['running', 'stopped'].each do |svc_status|
         context "with service_ensure #{svc_status}" do
-          let(:params) { { :service_ensure => svc_status } }
-          it { should contain_service('chronyd').with(
-            :ensure => svc_status,
-          )}
+          let(:params) { { service_ensure: svc_status } }
+
+          it {
+            is_expected.to contain_service('chronyd').with(
+              ensure: svc_status,
+            )
+          }
         end
       end
 
       [true, false].each do |svc_enable|
         context "with service_enabler #{svc_enable}" do
-          let(:params) { { :service_enable => svc_enable } }
-          it { should contain_service('chronyd').with(
-            :enable => svc_enable,
-          )}
+          let(:params) { { service_enable: svc_enable } }
+
+          it {
+            is_expected.to contain_service('chronyd').with(
+              enable: svc_enable,
+            )
+          }
         end
       end
     end
